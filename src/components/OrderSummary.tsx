8393101a869cd1340c2a461040406323d354fc95
aiman-waseem <aimanwaseem.aw26@gmail.com>
2024-10-04 18:06:03 +0500

    update

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -1,14 +1,44 @@
 import { useAppDispatch, useAppSelector } from '@/app/store/hooks'
 import React from 'react'
 import { Button } from './ui/button';
+import getStripe from '../lib/getStripe';
+import toast from 'react-hot-toast';
 
 const OrderSummary = () => {
   const cartProducts = useAppSelector((state)=> state.cart.cartItems)
-
+  console.log("Cart Product in Summary", cartProducts)
 const dispatch = useAppDispatch()
   // Calculate the total quantity
   const totalPrice = cartProducts.reduce((acc, item) => acc + item.qty * item.price, 0);
 
+   const handleCheckout = async () => {
+  try {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartProducts),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    await stripe.redirectToCheckout({ sessionId: data.id });
+  } catch (error) {
+    console.error('Checkout error:', error);
+    toast.error('Checkout failed. Please try again.');
+  }
+};
+
+  // console.log("Stripe key",process.env.NEXT_PUBLIC_STRIPE_KEY)
   return (
     <>
       {cartProducts.length > 0 && (
@@ -38,7 +68,9 @@ const dispatch = useAppDispatch()
             <p>Total Price: ${totalPrice}</p>
           </div>
 
-          <Button className='hover:bg-black hover:text-white rounded-sm m-5' variant={'outline'}>Proceed to Checkout</Button>
+          <Button className='hover:bg-black hover:text-white rounded-sm m-5' variant={'outline'}
+           onClick={handleCheckout}
+          >Proceed to Checkout</Button>
           </div>
        
       )}
